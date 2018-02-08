7ce8dd8865b0103c3d7c2ab0ceadde9279fe1463
Misha Petrov <modesau@gmail.com>
2018-02-08 10:16:09 -0800

    add method to count all coins

--- lib/coins.rb
+++ lib/coins.rb
@@ -3,16 +3,25 @@ class Change
   def initialize(coins)
     @coins = coins
   end
-
-  def quarters()
-    @coins / 25
+  def quarters(coins)
+    (coins / 25).floor
+  end
+  def dimes(coins)
+    (coins / 10).floor
+  end
+  def nickels(coins)
+    (coins / 5).floor
   end
 
-  def dimes()
-    @coins / 10
+  def calculate_change
+    total_coins = @coins
+    puts "#{quarters(total_coins)} quarters"
+    total_coins = total_coins - (quarters(total_coins) * 25)
+    puts "#{dimes(total_coins)} dimes"
+    total_coins = total_coins - (dimes(total_coins) * 10)
+    puts "#{nickels(total_coins)} nickels"
+    total_coins = total_coins - (nickels(total_coins) * 5)
+    puts "#{total_coins} pennies"
   end
 
-  def nickels()
-    @coins / 5
-  end
 end
--- spec/coins_spec.rb
+++ spec/coins_spec.rb
@@ -3,12 +3,12 @@ require('coins')
 
 describe('#coins') do
   it("returns number of quarters for given change") do
-    expect(change = Change.new(125).quarters()).to(eq(5))
+    expect(change = Change.new(125).quarters(125)).to(eq(5))
   end
   it("returns number of dimes for given change") do
-    expect(change = Change.new(20).dimes()).to(eq(2))
+    expect(change = Change.new(20).dimes(20)).to(eq(2))
   end
   it("returns number of nickels for given change") do
-    expect(change = Change.new(30).nickels()).to(eq(6))
+    expect(change = Change.new(30).nickels(30)).to(eq(6))
   end
 end
