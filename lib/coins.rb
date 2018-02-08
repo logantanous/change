47c77691939a0246b1feba6502f378a106d94972
Misha Petrov <modesau@gmail.com>
2018-02-08 09:29:22 -0800

    pass dimes and nickels test

--- lib/coins.rb
+++ lib/coins.rb
@@ -1,8 +1,18 @@
 class Change
+
   def initialize(coins)
     @coins = coins
   end
+
   def quarters()
     @coins / 25
+  end
+
+  def dimes()
+    @coins / 10
+  end
+
+  def nickels()
+    @coins / 5
   end
 end
--- spec/coins_spec.rb
+++ spec/coins_spec.rb
@@ -2,7 +2,13 @@ require('rspec')
 require('coins')
 
 describe('#coins') do
-  it("returns a word for any digit") do
+  it("returns number of quarters for given change") do
     expect(change = Change.new(125).quarters()).to(eq(5))
+  end
+  it("returns number of dimes for given change") do
+    expect(change = Change.new(20).dimes()).to(eq(2))
+  end
+  it("returns number of nickels for given change") do
+    expect(change = Change.new(30).nickels()).to(eq(6))
   end
 end
